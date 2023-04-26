d545ce7456edca6cc87b61d22487694858f16672
kwonnahyun <rnjsskgus4244@gmail.com>
2023-04-26 17:22:39 +0900

    framer-motion : vriants , hover, click, scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
 
-const Wrapper = styled.div`
-    height: 100vh;
+const Wrapper = styled(motion.div)`
+    height: 200vh;
     width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
 `;
+
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    background-color: white;
-    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-    start: { scale: 0 },
-    end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
-};
 function App() {
+    const x = useMotionValue(0);
+    const rotateZ = useTransform(x, [-800, 800], [-350, 360]);
+    const gradient = useTransform(
+        x,
+        [-800, 800],
+        [
+            "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+            "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+        ]
+    );
+    const { scrollYProgress } = useScroll();
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
     return (
-        <Wrapper>
-            <Box variants={myVars} initial="start" animate="end" />
+        <Wrapper style={{ background: gradient }}>
+            <Box drag="x" dragSnapToOrigin style={{ x, rotateZ, scale }} />
         </Wrapper>
     );
 }
